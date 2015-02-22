06969ca1f0fef2b79e7bc33f48b4502835128ff5
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-02-22 15:18:31 +0100

    Fixed map current location issue

--- scripts/geo-lookup.js
+++ scripts/geo-lookup.js
@@ -27,37 +27,34 @@
 
 var geoLookup = {
   initialize: function() {
-    this.mapLoad();
-    //this.locationEntered();
+    this.currLocation();
+    this.locationEntered();
   },
 
   //Gets current location of user
   currLocation: function() {
     function success(position) {
-      var cordinate = position.coords, 
-          latitude = cordinate.latitude,
-          longitude = cordinate.longitude,
-          currPosition = {lat: latitude, lng:  longitude};
-          console.log(currPosition);
-      return currPosition;
+      console.log(position);
+      var coords = position.coords;
+      geoLookup.mapLoad(coords.latitude, coords.longitude);
+      return coords;
     };
     navigator.geolocation.getCurrentPosition(success);
   },
 
   //Loads map with default location
-  mapLoad: function() {
+  mapLoad: function(lat, lng) {
     function initialize() {
-      console.log("I'm here");
       var mapOptions = {
-        center:  geoLookup.currLocation(),
-        zoom: 8,
+        center:  {lat: lat, lng: lng},
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.HYBRID
-      };      
+      };
       var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
       var marker=new google.maps.Marker({position:mapOptions.center});
       marker.setMap(map);
     }
-    google.maps.event.addDomListener(window, 'load', initialize);   
+    google.maps.event.addDomListener(window, 'load', initialize());   
   },
 
   //concatenates url with user's input
@@ -65,45 +62,40 @@ var geoLookup = {
     var location = $('#textInput').val().trim(),
         url = "http://api.openweathermap.org/data/2.5/weather?",
         fullUrl = url + "q=" + location;
+        console.log("full URL: ", fullUrl);
     return fullUrl;
 
-  }
+  },
 
   //Gets user's location
-  // locationEntered: function() {
-  //   $("#search").click(function(event){
-  //     event.preventDefault();
-  //     console.log("clicked");
-  //     geoLookup.weatherMap();     
-  //   });        
-  // },
+  locationEntered: function() {
+    $("#search").click(function(event){
+      event.preventDefault();
+      console.log("clicked");
+      geoLookup.weatherMap();     
+    });        
+  },
 
-  // //Calls weather api using location entered and sets map's location 
-  // weatherMap: function() {
-  //   var url = geoLookup.url();
-  //   console.log(url);
+  //Calls weather api using location entered and sets map's location 
+  weatherMap: function() {
+    var url = geoLookup.url();
+    console.log(url);
 
-  //   $.getJSON(url, function(reply){
-  //     console.log(reply);
-  //     var newLatitude = reply.coord.lat,
-  //         newLongitude = reply.coord.lon,
-  //         newLocation = {lat: newLatitude, lng:  newLongitude};
+    $.getJSON(url, function(reply){
+      console.log(reply);
+      var newLatitude = reply.coord.lat,
+          newLongitude = reply.coord.lon,
+          newLocation = {lat: newLatitude, lng:  newLongitude};
 
-  //     console.log("New location: ", newLocation)
-  //     //set map's view with current location entered
-  //     function initialize() {    
-  //     var mapOptions = {
-  //       center: newLocation,
-  //       zoom: 8,
-  //       mapTypeId: google.maps.MapTypeId.HYBRID
-  //     };      
-  //       var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  //       var marker=new google.maps.Marker({position:mapOptions.center});
-  //       marker.setMap(map);
-  //     }
-  //     google.maps.event.addDomListener(window, 'load', initialize);  
-  //   }); 
-  //}
+      console.log("New location: ", newLocation);
+
+      //set map's view with current location entered 
+      geoLookup.mapLoad(newLatitude, newLongitude);  
+
+      //Get weather details according to location
+      
+    }); 
+  }
   
 }
 
